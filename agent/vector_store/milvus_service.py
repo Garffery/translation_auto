13af643acc176b080f19c996f403c62d4e9a1285
garffery <13226133323@163.com>
2026-03-03 11:28:11 +0800

    混合检索

--- agent/vector_store/milvus_service.py
+++ agent/vector_store/milvus_service.py
@@ -1,10 +1,11 @@
-from pymilvus import MilvusClient
-
+from pymilvus import MilvusClient, AnnSearchRequest, RRFRanker
+from langchain_ollama import OllamaEmbeddings
 
 class MilvusService:
-    def __init__(self, uri:str, db_name:str, collection_name:str):
+    def __init__(self, uri:str, db_name:str, collection_name:str, embedding: OllamaEmbeddings):
         self.collection_name = collection_name
-        self.client = MilvusClient(uri = uri, db_name = db_name)
+        self.embedding = embedding
+        self.client = MilvusClient(uri=uri, db_name=db_name)
 
 
     async def query(self, collection_name:str, query_str:str):
@@ -24,5 +25,33 @@ class MilvusService:
         :param query_str:
         :return:
         """
+        query_token = self.embedding.embed_query(query_str)
+        search_params_dense = {
+            "metric_type": "COSINE",
+            "params": {"nprobe": 2}
+        }
+        request_dense = AnnSearchRequest([query_token], "vector", search_params_dense, limit=2)
+
+        search_params_bm25 = {
+            "metric_type": "BM25"
+        }
+
+        # Create a BM25 text search request
+        request_bm25 = AnnSearchRequest([query_str], "sparse", search_params_bm25, limit=2)
+
+        # Combine the two requests
+        reqs = [request_dense, request_bm25]
+
+        # Initialize the RRF ranking algorithm
+        ranker = RRFRanker(100)
+
+        # Perform the hybrid search
+        hybrid_search_res = self.client.hybrid_search(
+            collection_name="collection_hy_1",
+            reqs=reqs,
+            ranker=ranker,
+            limit=2,
+            output_fields=["text", "source"]
+        )
 
         pass
